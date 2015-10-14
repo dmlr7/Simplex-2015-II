4831d17b4d10b09cebbf00be29b485bc4d8af064
dmlr7 <leviatandemon@gmail.com>
2015-10-14 11:13:55 -0500

    fix files

---
+++ Test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import simplex
+def main():
+
+
+if __name__ == '__main__':
+    main()
--- simplex.py
+++ simplex.py
@@ -1,19 +1,19 @@
 import sys
-
-def lines(fp):
+class Simplex:
+  def lines(fp):
     print str(len(fp.readlines()))
 
-def toint(arr): # convert a string array to int array
+  def toint(arr): # convert a string array to int array
     if type(arr) is str :
         return map(int, arr.split())
     return map(int, arr)
 
-def tofloat(arr): # convert a string array to int array
+  def tofloat(arr): # convert a string array to int array
     if type(arr) is str :
         return map(float, arr.split())
     return map(float, arr)
 
-def main():
+  def main():
     with open(sys.argv[1]) as f :
         lines = f.readlines()
 
@@ -27,48 +27,15 @@ def main():
     z = {}
     for i in range(len(sidx)) :
         z[sidx[i]] = obj[i+1]
-    """
-    indxMatrizA = -1
-    indVarEntra=100
-    for i in range(len(sidx)):
-        if (z[sidx[i]] > 0 and sidx[i] <= indVarEntra ):
-            indVarEntra=sidx[i]
-            indxMatrizA=i
-            
-    ix = {}
-    indVarSale = 100
-    indymatrizA=-1;
-    for j in range(len(bidx)) :
-        ix[bidx[j]] = A[j][indxMatrizA]
-        if(A[j][indxMatrizA]<0 and b[j]/-(A[j][indxMatrizA]) <= indVarSale):
-            if(b[j]/-(A[j][indxMatrizA]) == indVarSale and bidx[j]<bidx[indymatrizA]):
-                indymatrizA=j
-            else:
-                indVarSale=b[j]/-(A[j][indxMatrizA])
-                indymatrizA=j
-                
-            
-    
-    if(indymatrizA >= 0):
-        aumento=float(b[indymatrizA]/-A[indymatrizA][indxMatrizA])   
-        
-        #print "entra id"
-        print sidx[indxMatrizA]  
-  
-        #print "sale id"
-        print bidx[indymatrizA]  
-        #print "objetivo"
-        number=obj[0] + aumento*z[sidx[indxMatrizA] ]
-        print round(number,4)
-    else: print "UNBOUNDED"
-    """
+
+
     init(bidx,sidx,b,A,obj)
 
     # pivoting
     # print len(lines) , A, m, n, bidx, sidx, b, obj
 
 
-def init(bidx,sidx,b,A,obj):
+  def init(bidx,sidx,b,A,obj):
     print(bidx,sidx,b,A,obj)
     if min(b) < 0:
         Ap = []
@@ -83,7 +50,7 @@ def init(bidx,sidx,b,A,obj):
     else:
         opti()
 
-def pivote(bidx,sidx,b,A,obj,z):
+  def pivote(bidx,sidx,b,A,obj,z):
     indxMatrizA = -1
     indVarEntra=100
     for i in range(len(sidx)):
@@ -118,8 +85,5 @@ def pivote(bidx,sidx,b,A,obj,z):
         print round(number,4)
     else: print "UNBOUNDED"
 
-def opti():
+  def opti():
     print("OPTIMI")
-
-if __name__ == '__main__':
-    main()
